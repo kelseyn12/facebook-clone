086bc2d3e78b7dd0edbaedaf723885aef1cb1a3e
kelsey nocek <kelseyn12@hotmail.com>
2022-03-09 22:26:16 -0600

    fixed repo

---
+++ fb-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    //BEM naming convention
+    <div className="app">
+
+      <Header />
+
+      {/*App body */}
+        {/* Sidebar */}
+        {/* Feed */ }
+    </div>
+  );
+}
+
+export default App;
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCHegOJyBptXTzQbOLEJpzj0eoCI0xv0hc",
+    authDomain: "facebook-clone-871a3.firebaseapp.com",
+    projectId: "facebook-clone-871a3",
+    storageBucket: "facebook-clone-871a3.appspot.com",
+    messagingSenderId: "696225024083",
+    appId: "1:696225024083:web:e22b81f5f01c74ebc49ea6",
+    measurementId: "G-2M04DFETKW"
+  };
