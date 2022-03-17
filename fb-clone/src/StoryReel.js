ddf19ef9d4e2a97d11a33ccdd3403adc0146c57f
kelsey nocek <kelseyn12@hotmail.com>
2022-03-17 11:35:49 -0500

    built storyreel and stories within storyreel

---
+++ fb-clone/src/StoryReel.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Story from './Story';
+import './StoryReel.css';
+
+function StoryReel() {
+  return (
+    <div className='storyReel'>
+        <Story 
+        image="https://scontent-msp1-1.xx.fbcdn.net/v/t1.6435-9/70288515_10157055650157672_8100359709539696640_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=fydyOutkDu4AX-J09mN&_nc_ht=scontent-msp1-1.xx&oh=00_AT8oKZB16gH7rRq3u-Gn44rqMmUeadjgaf_h9HSp_XeQVQ&oe=625A6539"
+        profileSrc="https://cdn-icons-png.flaticon.com/512/194/194938.png"
+        title="Suzy Sue" 
+        />
+        <Story
+        image="https://scontent-msp1-1.xx.fbcdn.net/v/t1.6435-9/32349124_10213470793536851_7360572865776713728_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=84a396&_nc_ohc=Ezw6B_vsgjsAX9s2AKS&_nc_ht=scontent-msp1-1.xx&oh=00_AT_79HtoaoGVDOEKzC1NsPg_gmuV68Grj4sPzGqb_Vp8CQ&oe=625913B1"
+        profileSrc="https://cdn-icons-png.flaticon.com/512/147/147144.png"
+        title="Bobbo Booley"
+        />
+        <Story 
+        image="https://scontent-msp1-1.xx.fbcdn.net/v/t31.18172-8/23004610_10214530871001181_3884701212174892900_o.jpg?_nc_cat=101&ccb=1-5&_nc_sid=cdbe9c&_nc_ohc=3jM0lPscwXQAX_J7Fbf&_nc_ht=scontent-msp1-1.xx&oh=00_AT9SYZ5YAe_HT7LL0B-NhQ-xN5rWR76GeHK3ihwJACxlQw&oe=62577F67"
+        profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuR6WDWiTUC4X8faeqmsSkpI1gWxujB07xvw&usqp=CAU"
+        title="Manilla Moy"
+        />
+        <Story 
+        image="https://scontent-msp1-1.xx.fbcdn.net/v/t31.18172-8/14324156_10154062768272672_7997701439492519053_o.jpg?_nc_cat=100&ccb=1-5&_nc_sid=cdbe9c&_nc_ohc=bmxvN9XrLicAX8ptAfS&_nc_ht=scontent-msp1-1.xx&oh=00_AT_31oXDMklkw6QdhXXwu3Iqch51ppEgiDbbLEYPG1gNgA&oe=625AB7FF"
+        profileSrc="https://cdn.iconscout.com/icon/free/png-256/avatar-1924065-1628715.png"
+        title="Gilly Gobo"
+        />
+        <Story
+        image="https://scontent-msp1-1.xx.fbcdn.net/v/t31.18172-8/13909366_10153949206797672_5113584824327248159_o.jpg?_nc_cat=108&ccb=1-5&_nc_sid=cdbe9c&_nc_ohc=1PioVs49lW0AX_YOUIh&_nc_ht=scontent-msp1-1.xx&oh=00_AT-IIFGzb4cIKIbaqB87CqQq-_WZ3Bkpvoz6jly2u1z3EA&oe=625AC0E9"
+        profileSrc="https://www.pngkit.com/png/detail/115-1150121_this-free-icons-png-design-of-female-avatar.png"
+        title="Holly Hotolo"
+        />
+    </div>
+  )
+}
+
+export default StoryReel;
