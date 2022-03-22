2075f8cd78998d7d8df48d8ed610717ac5d0f271
kelsey nocek <kelseyn12@hotmail.com>
2022-03-22 16:26:03 -0500

    added use state to input

--- fb-clone/src/MessageSender.js
+++ fb-clone/src/MessageSender.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar } from '@mui/material';
 import './MessageSender.css';
 import VideocamIcon from '@mui/icons-material/Videocam';
@@ -6,6 +6,9 @@ import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 function MessageSender() {
+    const [input, setInput] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
     const handleSubmit = e => {
         e.preventDefault();
     };
@@ -14,10 +17,16 @@ function MessageSender() {
         <div className="messageSender__top">
             <Avatar />
             <form>
-                <input className='messageSender__input'
+                <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)} 
+                className='messageSender__input'
                 placeholder={`What's on your mind?`} 
                 />
-                <input placeholder='image URL (Optional)'/>
+                <input 
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+                placeholder='image URL (Optional)'/>
 
                 <button onClick={handleSubmit} type="submit">
                     Hidden submit
