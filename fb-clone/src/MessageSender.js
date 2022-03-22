d923eb8eef96d0a3d3f0ec368b81d6faea645b9f
kelsey nocek <kelseyn12@hotmail.com>
2022-03-22 15:46:03 -0500

    added styling to form and live video, photo/video, and emoticon option to whats on your mind form

--- fb-clone/src/MessageSender.js
+++ fb-clone/src/MessageSender.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Avatar } from '@mui/material';
 import './MessageSender.css';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 function MessageSender() {
     const handleSubmit = e => {
@@ -12,7 +15,7 @@ function MessageSender() {
             <Avatar />
             <form>
                 <input className='messageSender__input'
-                placeholder={`What's on your mind`} 
+                placeholder={`What's on your mind?`} 
                 />
                 <input placeholder='image URL (Optional)'/>
 
@@ -23,7 +26,20 @@ function MessageSender() {
         </div>
 
         <div className="messageSender__bottom">
-        
+            <div className="messageSender__option">
+            <VideocamIcon style={{ color: "red "}} />
+            <h3>Live Video</h3>
+            </div>
+
+        <div className="messageSender__option">
+            <PhotoLibraryIcon style={{ color: "green" }} />
+            <h3>Photo/Video</h3>    
+        </div>
+
+        <div className="messageSender__option">
+            <InsertEmoticonIcon style={{ color: "orange" }} />
+            <h3>Feeling/Activity</h3>
+        </div>
         </div>
     </div>
   )
