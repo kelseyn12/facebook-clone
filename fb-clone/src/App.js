9c99a53997d5e247565167fdd5134078c706f73d
kelsey nocek <kelseyn12@hotmail.com>
2022-04-04 10:52:14 -0500

    added a login component

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,23 +1,32 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import Login from "./Login";
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widgets from './Widgets';
 
 function App() {
+  const user = null;
   return (
     //BEM naming convention
     <div className="app">
+      {!user ? (
+      <Login /> 
+      ) : (
 
+      <>
       <Header />
 
       <div className="app__body">
         <Sidebar />
         <Feed />
         <Widgets />
-        </div>  
+        </div> 
+      </> 
+      )}
     </div>
+    
   );
 }
 
