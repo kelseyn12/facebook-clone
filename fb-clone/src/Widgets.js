1bbc91b7c2569a8636f11db3818c5084fc7c3796
kelsey nocek <kelseyn12@hotmail.com>
2022-04-04 10:09:15 -0500

    added redtide iframe widget

--- fb-clone/src/Widgets.js
+++ fb-clone/src/Widgets.js
@@ -4,23 +4,8 @@ import './Widgets.css';
 function Widgets() {
   return (
     <div className='widgets'>
-        <iframe title='RedTideDuo'src="https://www.facebook.com/
-        plugins/page.php?
-        href=https%3A%2F%2Fwww.facebook.
-        com%2Fredtideduo%2F&
-        tabs=timeline&width=340&height=1500%25&
-        small_header=false&
-        adapt_container_width=true&
-        hide_cover=false&show_facepile=true&
-        appId" 
-        width="340" 
-        height="100%" 
-        style={{ border: 'none', overflow: 
-        'hidden' }} 
-        scrolling="no" 
-        frameborder="0" 
-        allow="encrypted-media">
-        </iframe>
+      <iframe title='redtideduo page' src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fredtideduo%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" width="340" height="500" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+       
     </div>
   )
 }
